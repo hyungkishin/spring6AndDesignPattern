7510f0d823acf804bd5a2f303efeb57e3ad9e5cb
hyungkishin <tkaqkeldk@nate.com>
2024-06-29 21:07:42 +0900

    feat: clock 을 이용한 시간 테스트

--- src/main/java/hyungkispring/hellospring/ObjectFactory.java
+++ src/main/java/hyungkispring/hellospring/ObjectFactory.java
@@ -7,12 +7,14 @@ import hyungkispring.hellospring.payment.PaymentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Clock;
+
 @Configuration
 public class ObjectFactory {
 
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(cachedExRateProvider());
+        return new PaymentService(cachedExRateProvider(), clock());
     }
 
     @Bean
@@ -23,6 +25,11 @@ public class ObjectFactory {
     @Bean
     public ExRateProvider exRateProvider() {
         return new WebApiExRateProvider();
+    }
+
+    @Bean
+    public Clock clock () {
+        return Clock.systemDefaultZone();
     }
 
 }
--- src/test/java/hyungkispring/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/hyungkispring/hellospring/payment/PaymentServiceSpringTest.java
@@ -1,26 +1,30 @@
 package hyungkispring.hellospring.payment;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestObjectFactory.class)
+@ContextConfiguration(classes = TestPaymentConfig.class)
 class PaymentServiceSpringTest {
 
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private Clock clock;
 
     @Autowired
     private ExRateProviderStub exRateProviderStub;
@@ -46,6 +50,17 @@ class PaymentServiceSpringTest {
 
         // 원화 환산 금액
         assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+    }
+
+    @Test
+    void validUntil() throws IOException {
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // valid until 이 prepare() 30 분 뒤로 설정됬는가?
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
     }
 
 }
