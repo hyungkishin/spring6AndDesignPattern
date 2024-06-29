b572a7e61ddd62ceab1b94191e0f8582446d168a
hyungkishin <tkaqkeldk@nate.com>
2024-06-29 18:21:37 +0900

    feat: PaymentService의 prepare 메서드 테스트 추가 prepare 메서드에 대한 단위 테스트를 PaymentServiceSpringTest에 추가 환율 정보와 원화 환산 금액이 올바른지 검증 ExRateProviderStub을 사용하여 다른 환율을 시뮬레이션 의존성 주입을 위한 TestObjectFactory 구성 추가

---
+++ src/test/java/hyungkispring/hellospring/payment/TestObjectFactory.java
@@ -0,0 +1,20 @@
+package hyungkispring.hellospring.payment;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+
+@Configuration
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new ExRateProviderStub(BigDecimal.valueOf(1000));
+    }
+}
