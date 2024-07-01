fab568f0012ce4a48f1b2983646b128cf2e31f56
hyungkishin <tkaqkeldk@nate.com>
2024-07-01 23:44:59 +0900

    feat: ApiTemplate 분리

--- src/main/java/hyungkispring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/hyungkispring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,47 +1,21 @@
 package hyungkispring.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import hyungkispring.hellospring.api.ApiExecutor;
+import hyungkispring.hellospring.api.ApiTemplate;
 import hyungkispring.hellospring.api.ErApiExRateExtractor;
-import hyungkispring.hellospring.api.ExRateExtractor;
-import hyungkispring.hellospring.api.SimpleApiExecutor;
+import hyungkispring.hellospring.api.HttpClientApiExecutor;
 import hyungkispring.hellospring.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebApiExRateProvider implements ExRateProvider {
+
+    ApiTemplate apiTemplate = new ApiTemplate();
 
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor()); // 콜백
-    }
-
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) { // 템플릿
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException ioException) {
-            throw new RuntimeException(ioException);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor()); // 콜백
     }
 
 }
