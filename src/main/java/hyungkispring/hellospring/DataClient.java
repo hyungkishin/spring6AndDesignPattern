c618f11da9875eacf77598b8a2affe90e997a557
hyungkishin <tkaqkeldk@nate.com>
2024-07-04 21:43:59 +0900

    feat: jpa 도입후, order 를 통한 저장

---
+++ src/main/java/hyungkispring/hellospring/DataClient.java
@@ -0,0 +1,29 @@
+package hyungkispring.hellospring;
+
+import hyungkispring.hellospring.order.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.math.BigDecimal;
+
+public class DataClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
+        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Order order = new Order("100", BigDecimal.TEN);
+        em.persist(order);
+
+        System.out.println(order);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+}
