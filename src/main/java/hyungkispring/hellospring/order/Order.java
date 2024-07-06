24276bddfd016f7f7b5e09d8bef093357888ff63
hyungkishin <tkaqkeldk@nate.com>
2024-07-06 23:09:34 +0900

    feat: 기술에 독립적인 애플리케이션 서비스

--- src/main/java/hyungkispring/hellospring/order/Order.java
+++ src/main/java/hyungkispring/hellospring/order/Order.java
@@ -10,15 +10,10 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 
 
-@Entity
-@Table(name = "orders")
 public class Order {
 
-    @Id
-    @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
